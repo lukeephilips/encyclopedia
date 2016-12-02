c004974e807f1419968c5f7cc23337ff9a6293e0
Luke <luke.e.philips@gmail.com>
2016-12-02 09:34:15 -0800

    pass unit testing for find Class method

--- lib/word.rb
+++ lib/word.rb
@@ -4,14 +4,27 @@ class Word
 
   def initialize (attributes)
     @name = attributes.fetch(:name)
+    @id = @@words.length+1
   end
   def save
     @@words.push(self)
+  end
+  def id
+    @id
   end
   def self.all
     @@words.sort! {| a,b | a.name <=> b.name}
   end
   def self.clear
     @@words = []
+  end
+  def self.find (identifier)
+    found_word = nil
+    @@words.each do |word|
+      if word.id.eql?(identifier)
+        found_word = word
+      end
+    end
+      found_word
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,12 +5,24 @@ require 'pry-nav'
 
 describe(Word) do
   before do
+    Word.clear
     @word0 = Word.new({:name => 'dog'})
     @word0.save
   end
   describe('#initialize') do
     it('displays a word') do
       expect(@word0.name).to eq 'dog'
+    end
+  end
+  describe('#find') do
+    it('displays a word based on its associated id') do
+      expect(Word.find(@word0.id)).to eq @word0
+    end
+    it('displays a word based on its associated id') do
+      expect(Word.find(1)).to eq @word0
+    end
+    it('displays a word based on its associated id') do
+      expect(Word.find(@word0.id).name).to eq 'dog'
     end
   end
 end
